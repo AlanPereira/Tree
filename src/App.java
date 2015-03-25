72388f40046bb900c271d4b3e69c2845c10f82a5
Alan Pereira <alan.silva@realide.com>
2015-03-25 18:50:00 -0300

    Add mais metodos

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
+import java.util.ArrayList;
+
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Tree<String> teste = new Tree<String>();
+		ArrayList<String> t = new ArrayList<String>();
+		for(int i= 0; i<10;i++){
+			t.add("!"+i);
+		}
+		
+		for(String j: t){
+			System.out.println(j);
+		}
 	}
 
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,24 +1,91 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Tree <E>{
 
 	private Node root;
-	private int size;
+	
 
 	public Tree(){
 		setRoot(new Node());
-		size = 0;
-	}
-
-	public int getSize() {
-		return size;
+		
+	}	
+	
+	public Tree<E> parent(){
+		Tree<E> tree = new Tree<E>();
+		tree.setRoot(this.getRoot().getParent());
+		
+		return tree;
+		
 	}
 	
-	public void addNode(E element, Node parent){
-		Node node = new Node();
-		node.setParent(parent);
-		parent.addChildren(node);
+	private Node find(E e, Node no){
+		Node n;
+		if(no.getElement().equals(e)){
+			return no;
+		}else if(!no.getChildren().isEmpty()){
+			return null;
+		}else {
+			int i =0;boolean ok =false;
+			do{
+				n = find(e, no.getChildren().get(i));
+				if(!(n==null)){
+					ok = true;
+				}
+				i++;
+			}while(ok || no.getChildren().size()<i);
+			
+			return n;
+		}
+	}
+	
+	
+	
+	public Tree<E> find(E e){
+		Node node = find(e, getRoot());
+		if(!node.equals(null)){
+			Tree<E> t =new Tree<E>();
+			t.setRoot(node);
+			return t;
+		}
+		return null;
+	}
+	
+	
+	public List<Tree<E>> children (){
+		ArrayList<Tree<E>> list = new ArrayList<Tree<E>>();
+		for(Node node:this.getRoot().getChildren()){
+			Tree<E> e = new Tree<E>();
+			e.setRoot(node);
+			list.add(e);
+		}
+		return list;
+	}
+	
+	
+	public boolean isExternal(){
+		return this.getRoot().getChildren().isEmpty();
+	}
+	
+	public boolean isInternal(){
+		return !this.isExternal();
+	}
+	
+	public boolean isRoot(){
+				
+		if(null==this.getRoot().getParent()){
+			return true;
+		}
+		return false;
+	}
 		
-	}//Será necessario? por default ele é sempre um parent
+	
+	public void add(E element, Tree<E> e){
+		
+		//
+		
+	}
 	
 	public void addParent (E e) {
 		Node novoNo = new Node(e);
@@ -70,14 +137,13 @@ public class Tree <E>{
 		
 	}
 
-	public Node getRoot() {
+	protected Node getRoot() {
 		return root;
 	}
 
-	public void setRoot(Node root) {
+	protected void setRoot(Node root) {
 		this.root = root;
 	}
-//	public Object getSubtree(Node node){
-//		return Tree().setRoot(this);
-//	}
+
+	
 }
